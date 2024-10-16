ed664c3fe75b045ff848ecad70401bddea917181
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-10-16 09:21:33 +0200

    Update requete_cinema_loic.sql
    Ex a, b, c, d finish

--- requete_cinema_loic.sql
+++ requete_cinema_loic.sql
@@ -1,23 +1,33 @@
 -- a. Informations d’un film (id_film) : titre, année, durée (au format HH:MM) et réalisateur 
 
-SELECT *
-FROM film
-WHERE id_film = 1;
+SELECT f.titre_film, f.sortie_film, f.duree_film, p.prenom_personne, p.nom_personne
+FROM film f
+INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
+INNER JOIN personne p ON r.id_personne = p.id_personne
+WHERE f.id_film = 1
+GROUP BY p.prenom_personne, p.nom_personne;
 
 -- b. Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)
 
-SELECT titre_film, duree_film
-FROM film
-WHERE duree_film > 135
-ORDER BY duree_film DESC;
+SELECT f.titre_film, f.duree_film
+FROM film f
+WHERE f.duree_film > 135
+ORDER BY f.duree_film DESC;
 
 -- c. Liste des films d’un réalisateur (en précisant l’année de sortie) 
 
--- SELECT 
+SELECT f.titre_film, f.sortie_film
+FROM film f
+WHERE f.id_realisateur = 1;
 
 -- d. Nombre de films par genre (classés dans l’ordre décroissant)
 
-
+SELECT g.libelle_genre, COUNT(f.id_film) AS nbFilms
+FROM film f
+INNER JOIN appartenir a ON f.id_film = a.id_film
+INNER JOIN genre g ON a.id_genre = g.id_genre
+GROUP BY g.libelle_genre
+ORDER BY nbFilms DESC;
 
 -- e. Nombre de films par réalisateur (classés dans l’ordre décroissant)
 
