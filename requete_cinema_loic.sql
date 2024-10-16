44d6629132664d4bc10eecf6469eb3c0fddd7d3e
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-10-16 09:27:43 +0200

    Update requete_cinema_loic.sql
    Ex e finish

--- requete_cinema_loic.sql
+++ requete_cinema_loic.sql
@@ -31,7 +31,12 @@ ORDER BY nbFilms DESC;
 
 -- e. Nombre de films par réalisateur (classés dans l’ordre décroissant)
 
-
+SELECT p.prenom_personne, p.nom_personne, COUNT(f.id_film) AS nbFilms
+FROM film f
+INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
+INNER JOIN personne p ON r.id_personne = p.id_personne
+GROUP BY p.prenom_personne, p.nom_personne
+ORDER BY nbFilms DESC;
 
 -- f. Casting d’un film en particulier (id_film) : nom, prénom des acteurs + sexe
 
