796025e6ed4b121e18c809dd5f6911df94609e26
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-10-18 15:09:00 +0200

    Begin functions
    listFilmsActeur() done

--- sql/requete_cinema_loic.sql
+++ sql/requete_cinema_loic.sql
@@ -124,9 +124,24 @@ GROUP BY f.id_film, p.id_personne, g.id_genre;
 
 -- detailReal()
 
-
+SELECT p.prenom_personne, p.nom_personne
+FROM personne p
+INNER JOIN realisateur r ON p.id_personne = r.id_personne
+WHERE r.id_realisateur = 1
+GROUP BY p.id_personne;
 
 -- listFilmsReal()
+
+SELECT f.titre_film, YEAR(f.sortie_film) AS sortie_film, g.libelle_genre, f.note_film
+FROM film f
+INNER JOIN realisateur r ON f.id_realisateur = r.id_realisateur
+INNER JOIN appartenir a ON f.id_film = a.id_film
+INNER JOIN genre g ON a.id_genre = g.id_genre
+WHERE r.id_realisateur = 5
+GROUP BY r.id_realisateur, f.id_film, g.id_genre
+ORDER BY f.note_film DESC, sortie_film DESC;
+
+-- listActeursFilm()
 
 
 
@@ -135,6 +150,7 @@ GROUP BY f.id_film, p.id_personne, g.id_genre;
 
 
 -- detailActeur()
+
 
 
 -- listActeurs()
